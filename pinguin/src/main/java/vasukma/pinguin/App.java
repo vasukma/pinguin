59b3141a26ee028c793e0a4724ee35b820aad1da
maximus <maximus@maximus-notebook>
2019-08-10 12:33:02 +0700

    The first commit!

---
+++ pinguin/src/main/java/vasukma/pinguin/App.java
@@ -0,0 +1,13 @@
+package vasukma.pinguin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
